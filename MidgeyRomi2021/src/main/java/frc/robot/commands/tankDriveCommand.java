9709087fdb7e21578de1035b7c15eb9cace63630
Midgey Midgey <midgeon.fitzgibbon@gmail.com>
2021-01-09 18:05:30 -0800

    Tank Test 1
    Probably Tank? Might work? Who knows?

---
+++ MidgeyRomi2021/src/main/java/frc/robot/commands/tankDriveCommand.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.RomiDrivetrain;
+import java.util.function.Supplier;
+
+//This is basically just a rehashing of arcadeDrive, just with variables that make sense to me for tankDrive
+public class tankDriveCommand extends CommandBase {
+  private final RomiDrivetrain m_romiDrivetrain;
+  private final Supplier<Double> m_leftSpeedSupplier;
+  private final Supplier<Double> m_rightSpeedSupplier;
+  
+  public tankDriveCommand(RomiDrivetrain romiDrivetrain, Supplier<Double> leftSpeedSupplier, Supplier<Double> rightSpeedSupplier) {
+    m_romiDrivetrain = romiDrivetrain;
+    m_leftSpeedSupplier = leftSpeedSupplier;
+    m_rightSpeedSupplier = rightSpeedSupplier;
+    addRequirements(romiDrivetrain);
+  }
+
+  @Override
+  public void initialize() {
+
+  }
+
+  @Override
+  public void execute() {
+    m_romiDrivetrain.tankDrive(m_leftSpeedSupplier.get(), m_rightSpeedSupplier.get());
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
